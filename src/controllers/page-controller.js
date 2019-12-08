61685c8ba4701da89f82d0b7849c069cbe043772
Vladimir Karetnikov <karetnikov.v.e@gmail.com>
2019-12-08 23:19:54 +0200

    f

--- src/controllers/page-controller.js
+++ src/controllers/page-controller.js
@@ -1,8 +1,5 @@
-import Sort, {SortType} from "../components/sort.js";
-import FilmsSection from '../components/films-section.js';
 import MovieCard from '../components/movie-card.js';
 import MoviePopup from '../components/movie-popup.js';
-import ShowMoreBtn from '../components/more-btn.js';
 import NoFilms from '../components/nofilms-msg.js';
 import {render, RenderPosition, KeyCodes, remove} from '../utils/render.js';
 import {topRatedMovies, topCommentedMovies} from '../mock/data.js';
@@ -11,9 +8,6 @@ export default class PageController {
   constructor(container) {
     this._container = container;
     this._noDataComponent = new NoFilms();
-    this._sortComponent = new Sort();
-    this._filmSectionComponent = new FilmsSection();
-    this._showMoreButtonComponent = new ShowMoreBtn();
   }
 
   renderFilmCards(movie, container, place) {
@@ -52,60 +46,14 @@ export default class PageController {
 
   render(movies) {
     const MOVIES_STARTING_COUNT = 5;
-    const SHOWING_MOVIES_COUNT_BY_BUTTON = 5;
-    const siteMainElement = document.querySelector(`.main`);
-
-    render(this._container, this._sortComponent, RenderPosition.BEFOREEND);
-    render(this._container, this._filmSectionComponent, RenderPosition.BEFOREEND);
-
     const mainMoviesContainer = document.querySelector(`.films-list > .films-list__container`);
+
     if (movies.length === 0) {
       render(mainMoviesContainer, this._noDataComponent, RenderPosition.AFTERBEGIN);
       document.querySelectorAll(`.films-list--extra`).forEach((el) => el.remove());
     } else {
-      const filmsListSection = document.querySelector(`.films-list`);
-      const defaultList = movies.slice();
-      let showingMoviesCount = MOVIES_STARTING_COUNT;
-
-      this._sortComponent.setSortTypeChangeHandler((sortType) => {
-        const filmsLoaded = mainMoviesContainer.querySelectorAll(`.films-list .film-card`).length;
-
-        switch (sortType) {
-          case SortType.DATE:
-            movies.sort((a, b) => b.date.slice(b.date.length - 4) - a.date.slice(a.date.length - 4));
-            break;
-          case SortType.RATING:
-            movies.sort((a, b) => b.rating - a.rating);
-            break;
-          case SortType.DEFAULT:
-            movies = defaultList.slice();
-            break;
-        }
-
-        mainMoviesContainer.innerHTML = ``;
-
-        movies.slice(0, filmsLoaded).forEach((movie) => {
-          this.renderFilmCards(movie, mainMoviesContainer, RenderPosition.BEFOREEND);
-        });
-      });
-
       movies.slice(0, MOVIES_STARTING_COUNT).forEach((movie) => {
         this.renderFilmCards(movie, mainMoviesContainer, RenderPosition.BEFOREEND);
-      });
-
-      render(filmsListSection, this._showMoreButtonComponent, RenderPosition.BEFOREEND);
-
-      const showMore = siteMainElement.querySelector(`.films-list__show-more`);
-      showMore.addEventListener(`click`, () => {
-        const prevMoviesCount = showingMoviesCount;
-        showingMoviesCount = showingMoviesCount + SHOWING_MOVIES_COUNT_BY_BUTTON;
-
-        movies.slice(prevMoviesCount, showingMoviesCount)
-        .forEach((movie) => this.renderFilmCards(movie, mainMoviesContainer, RenderPosition.BEFOREEND));
-
-        if (showingMoviesCount >= movies.length) {
-          showMore.remove();
-        }
       });
 
       const topRatedContainer = document.querySelector(`body > main > section > section:nth-child(2) > div`);
--- src/main.js
+++ src/main.js
@@ -1,16 +1,61 @@
+import {movies} from './mock/data.js';
 import MainNav from './components/main-nav.js';
 import Profile from './components/profile.js';
 import Footer from './components/footer.js';
 import PageController from './controllers/page-controller.js';
+import Sort, {SortType} from "./components/sort.js";
+import FilmsSection from './components/films-section.js';
+import ShowMoreBtn from './components/more-btn.js';
 import {render, RenderPosition} from './utils/render.js';
-import {movies} from './mock/data.js';
 
 const siteHeaderElement = document.querySelector(`.header`);
 render(siteHeaderElement, new Profile(), RenderPosition.BEFOREEND);
 const siteMainElement = document.querySelector(`.main`);
 render(siteMainElement, new MainNav(), RenderPosition.BEFOREEND);
+const sortComponent = new Sort();
+render(siteMainElement, sortComponent, RenderPosition.BEFOREEND);
+render(siteMainElement, new FilmsSection(), RenderPosition.BEFOREEND);
 render(document.body, new Footer(), RenderPosition.BEFOREEND);
-
 const controller = new PageController(siteMainElement);
+const filmsListSection = document.querySelector(`.films-list`);
+const mainMoviesContainer = document.querySelector(`.films-list > .films-list__container`);
+const SHOWING_MOVIES_COUNT_BY_BUTTON = 5;
+const defaultList = movies.slice();
+let showingMoviesCount = 5;
 
 controller.render(movies);
+
+sortComponent.setSortTypeChangeHandler((sortType) => {
+  const filmsLoaded = mainMoviesContainer.querySelectorAll(`.films-list .film-card`).length;
+  switch (sortType) {
+    case SortType.DATE:
+      movies.sort((a, b) => b.date.slice(b.date.length - 4) - a.date.slice(a.date.length - 4));
+      break;
+    case SortType.RATING:
+      movies.sort((a, b) => b.rating - a.rating);
+      break;
+    case SortType.DEFAULT:
+      movies = defaultList.slice();
+      break;
+  }
+
+  mainMoviesContainer.innerHTML = ``;
+
+  movies.slice(0, filmsLoaded).forEach((movie) => {
+    controller.renderFilmCards(movie, mainMoviesContainer, RenderPosition.BEFOREEND);
+  });
+});
+
+render(filmsListSection, new ShowMoreBtn(), RenderPosition.BEFOREEND);
+const showMore = siteMainElement.querySelector(`.films-list__show-more`);
+showMore.addEventListener(`click`, () => {
+  const prevMoviesCount = showingMoviesCount;
+  showingMoviesCount = showingMoviesCount + SHOWING_MOVIES_COUNT_BY_BUTTON;
+
+  movies.slice(prevMoviesCount, showingMoviesCount)
+  .forEach((movie) => controller.renderFilmCards(movie, mainMoviesContainer, RenderPosition.BEFOREEND));
+
+  if (showingMoviesCount >= movies.length) {
+    showMore.remove();
+  }
+});
