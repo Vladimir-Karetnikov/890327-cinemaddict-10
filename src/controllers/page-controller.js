d063b8fd263dedde8b89ced7cd944a62aa73b596
Vladimir Karetnikov <karetnikov.v.e@gmail.com>
2019-12-08 23:53:50 +0200

    ff

--- src/controllers/page-controller.js
+++ src/controllers/page-controller.js
@@ -1,6 +1,8 @@
 import MovieCard from '../components/movie-card.js';
 import MoviePopup from '../components/movie-popup.js';
 import NoFilms from '../components/nofilms-msg.js';
+import Sort, {SortType} from "../components/sort.js";
+import FilmsSection from '../components/films-section.js';
 import {render, RenderPosition, KeyCodes, remove} from '../utils/render.js';
 import {topRatedMovies, topCommentedMovies} from '../mock/data.js';
 
@@ -8,6 +10,8 @@ export default class PageController {
   constructor(container) {
     this._container = container;
     this._noDataComponent = new NoFilms();
+    this._sortComponent = new Sort();
+    this._filmSectionComponent = new FilmsSection();
   }
 
   renderFilmCards(movie, container, place) {
@@ -45,13 +49,37 @@ export default class PageController {
   }
 
   render(movies) {
+    render(this._container, this._sortComponent, RenderPosition.BEFOREEND);
+    render(this._container, this._filmSectionComponent, RenderPosition.BEFOREEND);
     const MOVIES_STARTING_COUNT = 5;
     const mainMoviesContainer = document.querySelector(`.films-list > .films-list__container`);
+    const defaultList = movies.slice();
 
     if (movies.length === 0) {
       render(mainMoviesContainer, this._noDataComponent, RenderPosition.AFTERBEGIN);
       document.querySelectorAll(`.films-list--extra`).forEach((el) => el.remove());
     } else {
+      this._sortComponent.setSortTypeChangeHandler((sortType) => {
+        const filmsLoaded = mainMoviesContainer.querySelectorAll(`.films-list .film-card`).length;
+
+        switch (sortType) {
+          case SortType.DATE:
+            movies.sort((a, b) => b.date.slice(b.date.length - 4) - a.date.slice(a.date.length - 4));
+            break;
+          case SortType.RATING:
+            movies.sort((a, b) => b.rating - a.rating);
+            break;
+          case SortType.DEFAULT:
+            movies = defaultList.slice();
+            break;
+        }
+
+        mainMoviesContainer.innerHTML = ``;
+
+        movies.slice(0, filmsLoaded).forEach((movie) => {
+          this.renderFilmCards(movie, mainMoviesContainer, RenderPosition.BEFOREEND);
+        });
+      });
       movies.slice(0, MOVIES_STARTING_COUNT).forEach((movie) => {
         this.renderFilmCards(movie, mainMoviesContainer, RenderPosition.BEFOREEND);
       });
